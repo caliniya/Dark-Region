956ad32ba3b5cfb3918f316ef27acd333a703948
caliniya <caliniya@outlook.com>
2025-03-12 10:24:04 +0800

    调试

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ val sdkHome: String = System.getenv("ANDROID_HOME")
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<Jar> {
---
+++ src/main/java/DarkRegion/DarkRegion.java
@@ -0,0 +1,10 @@
+package caliniya.DarkRegion;
+
+import mindustry.mod.Mod;
+import arc.util.Log;
+
+public class DarkRegion extends Mod{
+	static{
+        Log.info("已启动");
+    }
+}
